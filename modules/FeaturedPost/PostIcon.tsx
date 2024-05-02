8b1087b4ab7dd628fb90cd0a9878642d6562356e
Vanessa Coles <webdev@vcoles.com>
2024-05-02 11:45:42 -0700

    PostIcon - Update props.

--- modules/FeaturedPost/PostIcon.tsx
+++ modules/FeaturedPost/PostIcon.tsx
@@ -1,16 +1,16 @@
 import {FeaturedPostIconProps} from "@/utils/types/posts";
 
-export default function PostIcon( {children, iconStyles, iconSettings}: FeaturedPostIconProps) {
+export default function PostIcon( {children, styles, settings}: FeaturedPostIconProps) {
   return (
     <svg
-      className={iconStyles}
-      xmlns={iconSettings.xmlns}
-      width={iconSettings.width}
-      height={iconSettings.height}
-      viewBox={iconSettings.viewBox}
-      fill={iconSettings.fill}
-      stroke={iconSettings.stroke}
-      strokeWidth={iconSettings.strokeWidth}
+      className={styles}
+      xmlns={settings.xmlns}
+      width={settings.width}
+      height={settings.height}
+      viewBox={settings.viewBox}
+      fill={settings.fill}
+      stroke={settings.stroke}
+      strokeWidth={settings.strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
     >
